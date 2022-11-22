a7f986f08769f6c37668e1ba8194b1e10386b313
Will Farmer <wfarmer2@bu.edu>
2022-11-22 00:47:31 -0700

    Updated styling, format, comments, and error checks

--- javascript/numberCalculator.js
+++ javascript/numberCalculator.js
@@ -1,5 +1,7 @@
 /**
 * Checks to see if user input is a valid number or not. 
+* Validates input if it is not text to ensure text number is received.
+* TODO: Add validation against special characters (couldn't get it working)
 * @param userInput is the value a user is giving the prompt.
 */
 function numberCheck(userInput) {
@@ -9,7 +11,6 @@ function numberCheck(userInput) {
         while (userInput !== null || !isNaN(userInput)) {
             alert("Invalid number! Please enter a valid number.");
             userInput = prompt("Please enter a number.");
-
             // once number is received move on to next prompt
             if (userInput !== null || isNaN(userInput)) {
                 userInput = parseFloat(userInput);
@@ -28,31 +29,63 @@ function numberCheck(userInput) {
 
 /**
 * Prompts users to input two valid numbers and adds them together.
+* Converts prompt string value into float to combine and alert to the 
+* user. 
 */
 function numberCalculator() {
 
-    // prompt for first integer
+    // prompt for first number
     let firstUserInput = prompt("Please enter a number.");
 
     // convert input to valid format
     let firstNumber = numberCheck(firstUserInput);
 
-    // prompt for first integer
+    // prompt for first number
     let secondUserInput = prompt("Please enter a second number.");
 
     // convert input to valid format
     let secondNumber = numberCheck(secondUserInput);
 
-    // calculate the two integers
-    let combinedNumber = firstNumber + secondNumber
+    // calculate the two numbers
+    let combinedNumber = firstNumber + secondNumber;
 
     alert("The sum of your two numbers is: " + combinedNumber.toString());
 
-    // alert if number is a big 
+    // alert if number is big 
     if (combinedNumber > 10) {
-        alert("That is a big number!")
+        alert("That is a big number!");
     }
+    // alert if number is small
     else if (combinedNumber <= 10) {
-        alert("That is a small number!")
+        alert("That is a small number!");
+    }
+}
+
+/**
+* Repeat process for a user to keep using the calculator.
+* Makes the user type in YES or NO to either continue the calculator or exit the program
+* TODO: Cancel option to exit the program as another path to close calculator (couldn't get done)
+*/
+function repeatProcess() {
+    // prompt user to use the calculator again or not
+    let userSelection = prompt("Would you like to use the calculator again? Please type YES or NO.");
+
+    // loop until the user exits the calculator by typing in NO
+    while (userSelection !== null || isNaN(userName)) {
+
+        // if user wants to continue with the calculator again, call the numberCalculator function
+        if (userSelection.toUpperCase() == "YES") {
+            numberCalculator();
+            userSelection = prompt("Would you like to use the calculator again? Please type YES or NO.");
+        }
+        // if user wants to stop the calculator send a thank you message and close
+        else if (userSelection.toUpperCase() == "NO") {
+            alert("Thank you for using the Simple Calculator! Take care!");
+            break;
+        }
+        // reprompt the user if the input is not a YES or NO
+        else {
+            userSelection = prompt("Invalid choice! Please type YES or NO to use the calculator again.");
+        }
     }
 }
